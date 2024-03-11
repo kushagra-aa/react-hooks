e1af2555b429b78175b0430e64f83d80cf1f2f21
kushagra-aa <kushagraagnihotri24@gmial.com>
2024-03-11 15:29:09 +0530

    added hook logic : useDebounce

---
+++ useDebounce/src/useDebounce.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+function useDebounce(
+  callback: () => void,
+  dependencies: unknown[],
+  timeout?: number
+) {
+  const [isInitialMount, setIsInitialMount] = useState(true);
+  const [debounceTimeout, setDebounceTimeout] = useState<
+    string | number | NodeJS.Timeout | null
+  >(null);
+
+  useEffect(() => {
+    if (!isInitialMount) {
+      if (!timeout) {
+        callback(); // Call immediately if no timeout
+      } else {
+        if (debounceTimeout) clearTimeout(debounceTimeout); // Clear any existing timeout
+
+        const newDebounceTimeout = setTimeout(callback, timeout); // Set new timeout
+        setDebounceTimeout(newDebounceTimeout);
+      }
+    }
+
+    setIsInitialMount(false); // Mark as not initial mount
+
+    // Cleanup the timeout when the component unmounts
+    return () => {
+      if (debounceTimeout) {
+        clearTimeout(debounceTimeout); // Cleanup on unmount
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [...dependencies, timeout]);
+}
+
+export default useDebounce;
